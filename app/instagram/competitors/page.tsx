8fc41a9a8ed0a034a45254d130f95074fe33f1bd
Shaimalka <shailoop1@gmail.com>
2026-03-25 02:36:59 +0700

    refactor(competitors): apply cyberpunk CSS class replacements

--- app/instagram/competitors/page.tsx
+++ app/instagram/competitors/page.tsx
@@ -2,7 +2,7 @@ import CompetitorTracker from '@/components/CompetitorTracker'
 
 export default function CompetitorsPage() {
   return (
-    <div className="p-6">
+    <div className="cyber-bg-grid p-6">
       <CompetitorTracker />
     </div>
   )
